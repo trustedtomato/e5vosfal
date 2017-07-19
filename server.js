3d1b2b00580eca0b3ac796066c2231597be93436
System <none>
2017-07-19 15:39:06 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,31 +3,21 @@
 
 // init project
 var express = require('express');
+var low = require('lowdb');
 // setup a new database
-var Datastore = require('nedb'), 
-    // Security note: the database is saved to the file `datafile` on the local filesystem. It's deliberately placed in the `.data` directory
-    // which doesn't get copied if someone remixes the project.
-    db = new Datastore({ filename: '.data/datafile', autoload: true });
+// persisted using async file storage
+// Security note: the database is saved to the file `db.json` on the local filesystem.
+// It's deliberately placed in the `.data` directory which doesn't get copied if someone remixes the project.
+var db = low('.data/db.json', { storage: require('lowdb/lib/storages/file-async') });
 var app = express();
 
 // default user list
-var users = [
+db.defaults({ users: [
       {"firstName":"John", "lastName":"Hancock"},
       {"firstName":"Liz",  "lastName":"Smith"},
       {"firstName":"Ahmed","lastName":"Khan"}
-    ];
-
-db.count({}, function (err, count) {
-  console.log("There are " + count + " users in the database");
-  if(err) console.log("There's a problem with the database: ", err);
-  else if(count<=0){ // empty database so needs populating
-    // default users inserted in the database
-    db.insert(users, function (err, usersAdded) {
-      if(err) console.log("There's a problem with the database: ", err);
-      else if(usersAdded) console.log("Default users inserted in the database");
-    });
-  }
-});
+    ]
+  }).write();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -49,34 +39,38 @@ app.get("/users", function (request, response) {
 
 // creates a new entry in the users collection with the submitted values
 app.post("/users", function (request, response) {
-  db.insert({ firstName: request.query.fName, lastName: request.query.lName}, function (err, userAdded) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else if(userAdded) console.log("New user inserted in the database");
-  });
+  db.get('users')
+  .push({ firstName: request.query.fName, lastName: request.query.lName })
+  .write()
+  console.log("New user inserted in the database");
   response.sendStatus(200);
 });
 
 // removes entries from users and populates it with default users
 app.get("/reset", function (request, response) {
   // removes all entries from the collection
-  db.remove({}, { multi: true }, function (err) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else console.log("Database cleared");
-  });
+  db.get('users')
+  .remove()
+  .write()
+  console.log("Database cleared");
+  
   // default users inserted in the database
-  db.insert(users, function (err, usersAdded) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else if(usersAdded) console.log("Default users inserted in the database");
-  });
+  db.defaults({ users: [
+      {"firstName":"John", "lastName":"Hancock"},
+      {"firstName":"Liz",  "lastName":"Smith"},
+      {"firstName":"Ahmed","lastName":"Khan"}
+    ]
+  }).write();
   response.redirect("/");
 });
 
 // removes all entries from the collection
 app.get("/clear", function (request, response) {
-  db.remove({}, { multi: true }, function (err) {
-    if(err) console.log("There's a problem with the database: ", err);
-    else console.log("Database cleared");
-  });
+  // removes all entries from the collection
+  db.get('users')
+  .remove()
+  .write()
+  console.log("Database cleared");
   response.redirect("/");
 });
 
